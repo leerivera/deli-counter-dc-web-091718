50b4ba772b055ab705c5a5cbcd467a119a38fde2
leerivera80@gmail.com Rivera <leerivera80@gmail.com>
2018-09-01 22:53:41 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -12,7 +12,12 @@ def line(array)
   puts "#{message}"
   end 
 end 
-    
+def take_a_number(array, name)
+  array.push(name)
+  position = array.index(name)
+  puts "Welcome, #{name}. You are number #{array.index(name)+1} in line."
+  return name, position 
+end 
  
   
  
